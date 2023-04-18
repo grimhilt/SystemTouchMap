d39a770234859403e5082dcc9b67c24ab5f3db13
grimhilt <grimhilt@users.noreply.github.com>
2023-04-18 23:13:59 +0200

    get positions and clicks

---
+++ Makefile
@@ -0,0 +1,13 @@
+CC = gcc
+CFLAGS = -Wall -Wextra -std=c99
+LDFLAGS = 
+LDLIBS = -lX11 -linput -lm `pkg-config --cflags libinput` `pkg-config --libs libinput`
+
+SRC = main.c 
+OBJ = ${SRC:.c=.o}
+
+main: ${SRC}
+	${CC} ${SRC} ${CFLAGS} ${LDLIBS} -o main -g
+clean:
+	${RM} ${OBJ}   # remove object files
+	${RM} main     # remove main program
---
+++ main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <linux/input.h>
+#include <X11/Xlib.h>
+#include <stdbool.h>
+
+int main() {
+    Display *display = XOpenDisplay(NULL);
+    int screen_num = DefaultScreen(display);
+
+    Screen *screen = ScreenOfDisplay(display, screen_num);
+    printf("Screen size: %dx%d\n", screen->width, screen->height);
+
+    Window root_window = RootWindow(display, screen_num);
+    Window child_window;
+    int root_x, root_y, win_x, win_y;
+    unsigned int mask;
+
+    int fd;
+    struct input_event ie;
+
+    // change "event18" to the appropriate device for your system
+    fd = open("/dev/input/event18", O_RDONLY);
+    if (fd == -1) {
+        perror("Could not open input device");
+        exit(EXIT_FAILURE);
+    }
+
+    int count = 0;
+    int t = 0;
+
+    int xDown = 0;
+    int yDown = 0;
+
+    bool isTouch = false;
+    bool isClick = false;
+
+    while (read(fd, &ie, sizeof(struct input_event))) {
+        isTouch = (ie.type == EV_KEY && ie.code == BTN_TOUCH);
+        isClick = (ie.type == EV_KEY && ie.code == BTN_LEFT || ie.code == BTN_RIGHT && ie.value == 1);
+        if (isTouch || isClick) {
+            if (ie.value == 1) { // finger down
+                XQueryPointer(display, root_window, &root_window, &child_window, &root_x, &root_y, &win_x, &win_y, &mask);
+                xDown = root_x;
+                yDown = root_y;
+
+                count++;
+            } else { // finger up
+                XQueryPointer(display, root_window, &root_window, &child_window, &root_x, &root_y, &win_x, &win_y, &mask);
+                if (xDown == root_x && yDown == root_y) {
+                    t++;               
+                    printf("Mouse position: %d,%d, %i\n", root_x, root_y, t);
+                }
+                count--;
+            }
+        } 
+    }
+
+    close(fd);
+    XCloseDisplay(display);
+
+    return 0;
+}
